8e1bcd1aa8615b07fcbdca606ee877799235e779
宋涛杰 <stjworkemail@163.com>
2023-11-24 16:29:56 +0800

    添加配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    title: 'DotNet编程之路',
+    description: 'DOTNET编程之路博客文章',
+    theme: 'reco',
+    // locales: {
+    //     '/': {
+    //         lang: 'zh-CN'
+    //     }
+    // },
+    themeConfig: {
+        subSidebar: 'auto',
+        nav: [
+            { text: '首页', link: '/' },
+            { 
+                text: 'DotNet编程之路博客', 
+                items: [
+                    { text: 'Github', link: 'https://github.com/songtaojie' },
+                    { text: 'Gitee', link: 'https://gitee.com/songtaojie' }
+                ]
+            }
+        ],
+        sidebar: [
+            {
+                title: '欢迎学习',
+                path: '/',
+                collapsable: false, // 不折叠
+                children: [
+                    { title: "学前必读", path: "/" }
+                ]
+            },
+            {
+                title: "基础学习",
+                path: '/handbook/ConditionalTypes',
+                collapsable: false, // 不折叠
+                children: [
+                { title: "条件类型", path: "/handbook/ConditionalTypes" },
+                { title: "泛型", path: "/handbook/Generics" }
+                ],
+            }
+        ]
+    }
+}
